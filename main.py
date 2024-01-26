6b2f4b2d67efa79645037b294eb024798f0d9c2b
Cocotterooo <fcocoterooo@gmail.com>
2024-01-26 04:15:37 +0100

    Rework de la terminal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from connection import ConecctionMongoDB
+from colorama import Fore, Style
 
 load_dotenv()
 # Datos de conexión a MongoDB Atlas
@@ -17,35 +18,37 @@ directorio = 'Directorio_markdowns'
 
 
 # Recorre todos los archivos en el directorio especificado
-print('Archivos markdown encontrados:')
+print(f'{Style.BRIGHT}# Archivos markdown encontrados:{Style.NORMAL}\n | ID | Nombre de Archivo |')
 for file_id, filename in enumerate(os.listdir(directorio)):
     if filename.endswith('.md'):
-        print(f'{file_id+1}. {filename}')
+        print(f'   {Fore.YELLOW}{Style.BRIGHT}{file_id+1}.{Fore.RESET}{Style.NORMAL}   {filename}')
+
+print(f'\n {Fore.BLACK}{Style.BRIGHT}0. {Style.NORMAL}si no quieres almacenar ningún archivo.{Fore.RESET}')
 
 error = True
 while error:
     try:
-        file_id = int(input('¿Qué archivo desea almacenar en la Base de Datos? '))
+        file_id = int(input(f'{Style.BRIGHT}#{Style.NORMAL} Introduce la ID del archivo que deseas almacenar: '))
         if file_id > len(os.listdir(directorio)) or file_id < 0:
             print('¡Debes ingresar un número válido!')
         else:
             error = False
     except ValueError:
-        print('¡Debes ingresar un número!')
+        print(f'{Fore.RED}{Style.BRIGHT}ERROR{Fore.RESET}{Style.NORMAL}: Debes ingresar un correspondiente a un archivo.')
 
 if file_id == 0:
-    print('¡No se almacenó ningún archivo en la Base de Datos!')
+    print(f'{Fore.GREEN}{Style.BRIGHT}OK{Fore.RESET}{Style.NORMAL}: ¡No se almacenó ningún archivo!')
     exit()
 
 filename = os.listdir(directorio)[file_id-1]
 if connection_markdown.count_documents('name', filename) > 0:
-    print('ERROR: El archivo ya existe en la Base de Datos')
+    print(f'{Fore.RED}{Style.BRIGHT}ERROR{Fore.RESET}{Style.NORMAL}: El archivo ya existe en la Base de Datos')
 else:
     with open(os.path.join(directorio, filename), 'r') as file: # Lee el archivo
         content = file.read() 
     # Almacena el contenido del archivo en MongoDB
     document = {'name': filename, 'content': content}
     connection_markdown.upload_document(document)
-    print('¡Archivos almacenados con éxito en MongoDB Atlas!')
+    print(f'{Fore.GREEN}{Style.BRIGHT}OK{Fore.RESET}{Style.NORMAL}: ¡Archivos almacenados con éxito en MongoDB Atlas!')
 
 exit()
