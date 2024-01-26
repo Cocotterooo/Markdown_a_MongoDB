b79a65834e42c9cfa423653e7767b07081558e48
Cocotterooo <fcocoterooo@gmail.com>
2024-01-26 03:49:58 +0100

    Añadida comprobación de si ya existe ese archivo en la base de datos

--- connection.py
+++ connection.py
@@ -2,13 +2,13 @@ import os
 from pymongo import MongoClient
 
 class ConecctionMongoDB:
-    def __init__(self, user, password, db, collection, cluster):
+    def __init__(self, user, password, cluster, db, collection):
         self.__user = user
         self.__password = password
         self.__cluster = cluster
         self.__database_name = db
-        self.__collection = collection
-        self.__uri_conexion = f'mongodb+srv://{self.__user}:{self.__password}@{self.__cluster}/{self.__database}?retryWrites=true&w=majority'
+        self.__collection_name = collection
+        self.__uri_conexion = f'mongodb+srv://{self.__user}:{self.__password}@{self.__cluster}/{self.__database_name}?retryWrites=true&w=majority'
         self.__client = MongoClient(self.__uri_conexion)
         self.__db = self.__client[self.__database_name]
         self.__collection = self.__db[self.__collection_name]
@@ -16,17 +16,14 @@ class ConecctionMongoDB:
     def upload_document(self, document):
         self.__collection.insert_one(document)
     
-    def obtener_documentos(self):
-        return self.__collection.find()
-    
-    def obtener_documento(self, key, id):
+    def obtain_document(self, key, id):
         return self.__collection.find_one({key: id})
     
-    def actualizar_documento(self, key, id, documento):
+    def update_document(self, key, id, documento):
         self.__collection.update_one({key: id}, {'$set': documento})
     
-    def eliminar_documento(self, key, id,):
+    def delete_document(self, key, id,):
         self.__collection.delete_one({key: id})
     
-    def eliminar_documentos(self):
-        self.__collection.delete_many({})
+    def count_documents(self, key, id):
+        return self.__collection.count_documents({key: id})
--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import os
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from connection import ConecctionMongoDB
 
 load_dotenv()
 # Datos de conexión a MongoDB Atlas
-usuario = os.getenv('MONGODB_USER')
-contraseña = os.getenv('MONGODB_PASS')
-nombre_base_datos = os.getenv('MONGODB_DB')
-nombre_coleccion = os.getenv('MONGODB_COLLECTION')
+user = os.getenv('MONGODB_USER')
+password = os.getenv('MONGODB_PASS')
+database = os.getenv('MONGODB_DB')
+collection = os.getenv('MONGODB_COLLECTION')
 cluster = os.getenv('MONGODB_CLUSTER')
 
-# Conexión a MongoDB Atlas
-uri_conexion = f'mongodb+srv://{usuario}:{contraseña}@{cluster}/{nombre_base_datos}?retryWrites=true&w=majority'
-client = MongoClient(uri_conexion)
-db = client[nombre_base_datos]
-collection = db[nombre_coleccion]
+connection_markdown = ConecctionMongoDB(user, password, cluster, database, collection)
 
 # Directorio donde se encuentran los archivos markdown
 directorio = 'Directorio_markdowns'
@@ -42,10 +38,14 @@ if file_id == 0:
     exit()
 
 filename = os.listdir(directorio)[file_id-1]
-with open(os.path.join(directorio, filename), 'r') as file:
-    contenido = file.read()
+if connection_markdown.count_documents('name', filename) > 0:
+    print('ERROR: El archivo ya existe en la Base de Datos')
+else:
+    with open(os.path.join(directorio, filename), 'r') as file: # Lee el archivo
+        content = file.read() 
     # Almacena el contenido del archivo en MongoDB
-    documento = {'name': filename, 'content': contenido}
-    collection.insert_one(documento)
+    document = {'name': filename, 'content': content}
+    connection_markdown.upload_document(document)
+    print('¡Archivos almacenados con éxito en MongoDB Atlas!')
 
-print('¡Archivos almacenados con éxito en MongoDB Atlas!')
+exit()
